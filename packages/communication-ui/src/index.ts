c2f98d60c0153a2565152fe4fac9805862d71aa1
James Burnside <mail@jamesburnside.com>
2021-03-30 12:53:17 -0700

    change file

--- packages/communication-ui/src/index.ts
+++ packages/communication-ui/src/index.ts
@@ -1,5 +1,9 @@
 // © Microsoft Corporation. All rights reserved.
 
+/**
+ * This should be converged with release.index.ts once we have fully removed non-release exports.
+ */
+
 export * from './components';
 export * from './composites';
 export * from './consumers';
