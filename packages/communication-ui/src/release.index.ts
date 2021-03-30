3ac71cb763a77065d80b87ce2a3bb8b6e65befb9
James Burnside <mail@jamesburnside.com>
2021-03-30 12:45:35 -0700

    create release index.ts we can use for generating the api while we refactor/remove the non-release items

---
+++ packages/communication-ui/src/release.index.ts
@@ -0,0 +1,7 @@
+// © Microsoft Corporation. All rights reserved.
+
+export * from './components';
+export * from './composites';
+export * from './constants';
+export * from './types';
+export * from './utils';
